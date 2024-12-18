d7bb6abf031b0a103d5a754aa3c9a785ac268f2d
unknown <hieptram40@gmail.com>
2024-12-18 22:31:17 +0700

    Route update

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,39 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
-// import Layout from "./components/Layout"
-// import Home from "./pages/Home/Home.jsx"
-// import Members from "./pages/Members"
-// import Login from "./pages/Login/Login.jsx"
-// import Homes from "./pages/Home/Homes.jsx"
-// import NotFound from "./pages/NotFound/NotFound.jsx" // Import trang NotFound
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route element={<Layout/>}>
-//           <Route path="/" index element={<Homes/>}/>
-//           <Route path="home/:homeId" element={<Home/>}/>
-//           <Route path="members" element={<Members />}/>
-//         </Route>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="*" element={<NotFound />} /> {/* Thêm route cho trang 404 */}
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App;
-
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"
+import { useEffect } from "react"
+import Layout from "./components/Layout"
 import Home from "./pages/Home/Home.jsx"
 import Members from "./pages/Members"
 import Login from "./pages/Login/Login.jsx"
 import Homes from "./pages/Home/Homes.jsx"
-import NotFound from "./pages/NotFound/NotFound.jsx" // Import trang NotFound
+import NotFound from "./pages/NotFound/NotFound.jsx"
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Xử lý reload trang ở đây
+    console.log("URL đã thay đổi:", location.pathname);
+  }, [location]);
+
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+      <Route element={<Layout/>}>
         <Route path="/" index element={<Homes/>}/>
         <Route path="home/:homeId" element={<Home/>}/>
         <Route path="members" element={<Members />}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} /> {/* Thêm route cho trang 404 */}
-      </Routes>
-    </BrowserRouter>
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+export default AppWrapper;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,52 +1,3 @@
-// import { useParams } from 'react-router-dom';
-// import DataStatics from './DataStatics/DataStatics.jsx'
-// import MyRoom from './MyRoom/MyRoom.jsx'
-// import SensorData from './SensorData/SensorData.jsx'
-// import Devices from './Device/Devices.jsx'
-// import WeatherForecast from './WeatherForecast/WeatherForecast.jsx'
-// import { useEffect, useState } from 'react';
-// import getUserHome from '../../apis/Homes/GetUserHome';
-
-// const Home = () => {
-//   const { homeId } = useParams();
-
-//   const [homeData, setHomeData] = useState(null);
-
-//   useEffect(() => {
-//     const fetchHomeData = async () => {
-//       try {
-//         const data = await getUserHome(homeId);
-//         setHomeData(data.data);
-//       } catch (error) {
-//         console.error('Error fetching home data:', error);
-//       }
-//     };
-
-//     fetchHomeData();
-//   }, [homeId]);
-
-//   if (!homeData) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div className='p-5'>
-//       <div className='grid grid-cols-2 gap-4 pb-5'>
-//         <div className='grid grid-cols-2 gap-4'>
-//           <SensorData homeId={homeId} homePodId={homeData.homePodId} />
-//           <WeatherForecast homeId={homeId} homePodId={homeData.homePodId} />
-//         </div>
-//         <DataStatics homeId={homeId} homePodId={homeData.homePodId} className="" />
-//         <Devices homeId={homeId} homePodId={homeData.homePodId} />
-//       </div>
-//       <MyRoom homeId={homeId} />
-//     </div>
-//   )
-// }
-
-// export default Home;
-
-
 import { useParams } from 'react-router-dom';
 import DataStatics from './DataStatics/DataStatics.jsx'
 import MyRoom from './MyRoom/MyRoom.jsx'
@@ -55,8 +6,6 @@ import Devices from './Device/Devices.jsx'
 import WeatherForecast from './WeatherForecast/WeatherForecast.jsx'
 import { useEffect, useState } from 'react';
 import getUserHome from '../../apis/Homes/GetUserHome';
-import Header from '../../components/Header';
-import Sidebar from '../../components/Sidebar';
 
 const Home = () => {
   const { homeId } = useParams();
@@ -81,24 +30,18 @@ const Home = () => {
   }
 
   return (
-    <div className='flex'>
-      <Sidebar />
-      <div className='w-full ml-16 md:ml-56'>
-        <Header />
-        <div className='p-5'>
-          <div className='grid grid-cols-2 gap-4 pb-5'>
-            <div className='grid grid-cols-2 gap-4'>
-              <SensorData homeId={homeId} homePodId={homeData.homePodId} />
-              <WeatherForecast homeId={homeId} homePodId={homeData.homePodId} />
-            </div>
-            <DataStatics homeId={homeId} homePodId={homeData.homePodId} className="" />
-            <Devices homeId={homeId} homePodId={homeData.homePodId} />
-          </div>
-          <MyRoom homeId={homeId} />
+    <div className='p-5'>
+      <div className='grid grid-cols-2 gap-4 pb-5'>
+        <div className='grid grid-cols-2 gap-4'>
+          <SensorData homeId={homeId} homePodId={homeData.homePodId} />
+          <WeatherForecast homeId={homeId} homePodId={homeData.homePodId} />
         </div>
+        <DataStatics homeId={homeId} homePodId={homeData.homePodId} className="" />
+        <Devices homeId={homeId} homePodId={homeData.homePodId} />
       </div>
+      <MyRoom homeId={homeId} />
     </div>
-  );
-};
+  )
+}
 
 export default Home;
