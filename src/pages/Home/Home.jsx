f5da74b155d67bd54eb8dc9610dd3bff076f4482
unknown <hieptram40@gmail.com>
2024-12-18 21:36:45 +0700

    statging init

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,32 +12,32 @@ const Home = () => {
 
   const [homeData, setHomeData] = useState(null);
 
-  useEffect(() => {
-    const fetchHomeData = async () => {
-      try {
-        const data = await getUserHome(homeId);
-        setHomeData(data.data);
-      } catch (error) {
-        console.error('Error fetching home data:', error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchHomeData = async () => {
+  //     try {
+  //       const data = await getUserHome(homeId);
+  //       setHomeData(data.data);
+  //     } catch (error) {
+  //       console.error('Error fetching home data:', error);
+  //     }
+  //   };
 
-    fetchHomeData();
-  }, [homeId]);
+  //   fetchHomeData();
+  // }, [homeId]);
 
-  if (!homeData) {
-    return <div>Loading...</div>;
-  }
+  // if (!homeData) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div className='p-5'>
       <div className='grid grid-cols-2 gap-4 pb-5'>
         <div className='grid grid-cols-2 gap-4'>
-          <SensorData homeId={homeId} homePodId={homeData.homePodId} />
-          <WeatherForecast homeId={homeId} homePodId={homeData.homePodId} />
+          <SensorData homeId={homeId}  />
+          <WeatherForecast homeId={homeId}  />
         </div>
-        <DataStatics homeId={homeId} homePodId={homeData.homePodId} className="" />
-        <Devices homeId={homeId} homePodId={homeData.homePodId} />
+        <DataStatics homeId={homeId}  className="" />
+        <Devices homeId={homeId}  />
       </div>
       <MyRoom homeId={homeId} />
     </div>
