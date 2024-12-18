842db72e5503fe7d52f93cb78989b5d7b3bcf373
unknown <hieptram40@gmail.com>
2024-12-18 21:26:51 +0700

    updated sensor setting

--- src/apis/Homes/GetUserHome.js
+++ src/apis/Homes/GetUserHome.js
@@ -4,6 +4,7 @@ import { API_ROOT } from '../../utils/constants';
 const getUserHomes = async (homeId) => {
   try {
     const token = localStorage.getItem('token');
+    console.log('token', token);
     const response = await axios.get(`${API_ROOT}/api/v1/home/${homeId}`, {
       headers: {
         'Content-Type': 'application/json',
